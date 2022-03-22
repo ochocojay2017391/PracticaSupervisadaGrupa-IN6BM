50bde6b0701aaa93a4ab13e332aac08ba052f083
Informatica Kinal <informatica@ALUMNOS.local>
2022-03-22 08:36:51 -0600

    Se agregan componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { DashboardComponent} from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'registrar', component: RegistrarComponent},
+  { path: 'inicio', component: InicioComponent},
+  { path: 'dashboard', component: DashboardComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
