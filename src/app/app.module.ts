4d0487d4e5ee6d872cfbe6d95114ba1a26699841
Informatica Kinal <informatica@ALUMNOS.local>
2022-03-22 08:14:48 -0600

    Se agregan componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrarComponent,
+    InicioComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
